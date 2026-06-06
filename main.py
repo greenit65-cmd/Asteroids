c46547d5f2b2e9874e894b2424a28ae1c90a3a29
BK Saw <greenit65@gmail.com>
2026-06-06 17:22:56 +0800

    CH3: Player L3:Moving Around

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
-def main():
+def main() -> None:
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-     
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+
     while True:
         log_state()
 
@@ -17,8 +22,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
              
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS and calculate the delta time
+        dt = clock.tick(60) / 1000.0
+
 
 if __name__ == "__main__":
     main()
